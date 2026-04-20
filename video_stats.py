9de726a0ae49bc39dff6fe12220f69a0fc11a19d
zoltek <jaroslaw.podwika@gmail.com>
2026-04-20 17:23:11 +0200

    Added git_video_ids function

--- video_stats.py
+++ video_stats.py
@@ -8,6 +8,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 
 def get_playlist_id():
@@ -36,5 +37,41 @@ def get_playlist_id():
         raise e
 
 
+def get_video_ids(playlistId):
+
+    video_ids = []
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+
+    try:
+        while True:
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken:
+                break
+
+        return video_ids
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    get_video_ids(playlistId)
