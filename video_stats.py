e27cfa548f25ce1c2e2e691d84da6414726529e1
zoltek <jaroslaw.podwika@gmail.com>
2026-04-20 17:57:07 +0200

    Added save_to_json function

--- video_stats.py
+++ video_stats.py
@@ -3,6 +3,7 @@ import json
 
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path="./.env")
 
@@ -29,7 +30,7 @@ def get_playlist_id():
         channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"][
             "uploads"
         ]
-        print(channel_playlistId)
+        # print(channel_playlistId)
 
         return channel_playlistId
 
@@ -115,7 +116,15 @@ def extract_video_data(video_ids):
         raise e
 
 
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
+
 if __name__ == "__main__":
     playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
-    print(extract_video_data(video_ids))
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
